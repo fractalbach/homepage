78ae73cc54d1a5241f749ef5cfa5f3972fc04791
fractalbach <fractalbach@gmail.com>
2022-09-05 03:52:55 -0700

    added descriptions, changed titles

--- build.py
+++ build.py
@@ -6,15 +6,35 @@ template_filepath = "templates/main.html.template"
 template = None
 
 content_directory = 'content'
-content_filenames = ['index', 'projects', 'contact']
+
+all_pages = [
+	{
+		'filename': 'index',
+		'TITLE': 'Home',
+		'DESCRIPTION': "Chris Achenbach's Homepage!",
+	},
+	{
+		'filename': 'projects',
+		'TITLE': 'Projects',
+		'DESCRIPTION': "Chris Achenbach's awesome project page!",
+	},
+	{
+		'filename': 'contact',
+		'TITLE': 'Contact',
+		'DESCRIPTION': "Contact Me!",
+	}
+]
+	
+
 
 
 with open(template_filepath, 'r') as f:
     template = Template(f.read())
 
 
-for filename in content_filenames:
+for page in all_pages:
     
+    filename = page['filename']
     input_filepath = content_directory + '/' + filename
     output_filepath = filename + '.html'
     content = ''
@@ -23,7 +43,8 @@ for filename in content_filenames:
         content = f.read()
 
     output = template.substitute({
-        'SUBTITLE': filename.capitalize(),
+        'TITLE': f"Achenbach - {page['TITLE']}",
+    	'DESCRIPTION': page['DESCRIPTION'],
         'MAIN': content,
     })
 
